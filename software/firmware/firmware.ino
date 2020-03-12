fa51d6a61cb12d25aa86c2305268137dd6ecebdd
pcremades <pablocremades@gmail.com>
2020-03-12 16:35:58 -0300

    Subo datos del sensor de color tomados en la prueba con colorante. Actualizo firmware: usa Timer2 para programar intervalo entre mediciones.
    Falta programar el tema de calibración y mediciones propiamente dichas.

--- software/firmware/firmware.ino
+++ software/firmware/firmware.ino
@@ -3,7 +3,11 @@
 #include <max6675.h>
 
 
-
+/*************************************************************
+Notes:
+- Timer 2 is used as clock. This means that PWM on pins 3 and 11
+cannot be used.
+*************************************************************/
 /* Sensor de color TCS34725 */
 /* Connect SCL    to analog 5
    Connect SDA    to analog 4
@@ -29,8 +33,19 @@ Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS347
 /* Inicializamos termocupla */
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
+String inputString;
+boolean stringComplete = false;
+
+//Tiempo en segundos que pasa entre medicion y medicion
+#define  TIMERseconds  30
+void setTimer();
+int count=0;
+boolean flag=false;
+void parseCommand();
+
 void setup(){
- Serial.begin(115200);
+ Serial.begin(9600);
+ inputString.reserve(50);
  
  if (tcs.begin()) {
     Serial.println("Found sensor");
@@ -38,16 +53,18 @@ void setup(){
     Serial.println("No TCS34725 found ... check your connections");
     while (1);
   }
+  
+ setTimer();
 }
 
+int i = 0;
 void loop(){
-  uint16_t r, g, b, c, colorTemp, lux;
-
-  for( int i=0; i<256; i+=1 ){
-   analogWrite(3, i);
-   Serial.println(i);
-   delay(500);
-   tcs.getRawData(&r, &g, &b, &c);
+  uint16_t r, g, b, c;
+  
+   analogWrite(9, i);
+   //Serial.println(i);
+   delay(50);
+   /*tcs.getRawData(&r, &g, &b, &c);
    Serial.print("R: "); Serial.print(r, DEC); Serial.print(" ");
    Serial.print("G: "); Serial.print(g, DEC); Serial.print(" ");
    Serial.print("B: "); Serial.print(b, DEC); Serial.print(" ");
@@ -55,6 +72,41 @@ void loop(){
   Serial.println(" ");
   
     Serial.print("**C = "); 
-   Serial.println(thermocouple.readCelsius());
+   Serial.println(thermocouple.readCelsius());*/
+   
+   if( flag ){
+     Serial.println(millis());
+     flag = false;  
+ }
+     
+   if( stringComplete ){
+      Serial.println(inputString);
+      parseCommand();
+      inputString = "";
+      stringComplete = false;
+   }
+   
+   i++;
+   if( i > 255 )
+     i = 0;
+}
+
+//Set Timer 2 to work as clock to set interval between measurements.
+void setTimer(){
+  TCCR2A &= 0x0;  //CTC mode
+  TCCR2A |= 0x2;
+  TCCR2B &= !(0x0F); //Clear prescaler settings
+  TCCR2B |= 0x7; //Fcpu/1024 -> 16Mhz/1024 = 15625
+  OCR2A = 125;  //Contar 125 pulsos. -> 15625/125 = 125
+  TIMSK2 |= (1 << OCIE2A); // Enable CTC interrupt 
+}
+
+//This routine is executed once every 1/125 seconds (see calculations above).
+//Then, every 124 calls to this function we get 1 second.
+ISR(TIMER2_COMPA_vect){
+  count++;
+  if(count == 124*TIMERseconds){
+    count = 0;
+    flag = true;
   }
 }
